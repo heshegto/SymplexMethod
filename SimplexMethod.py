726a0ee483bc8b19b36a3188c1c4c1ef365714fc
heshegto <heshkin@gmail.com>
2023-07-07 02:33:09 +0300

    Put class simplex method in another file. Made code more readable

---
+++ SimplexMethod.py
@@ -0,0 +1,107 @@
+from numpy import zeros
+
+
+class SimplexMethod:
+    """Решение задачи линейного программирования в каноническом виде"""
+    # Канонический вид это когда:
+    # 1) решается задача поиска максимума целевой функции
+    # 2) неравенства принимают ввид <тут коэффициенты> <= <тут константа>
+
+    def __init__(self, variables: int, inequalities: int) -> None:
+        """В функции инициализации создается таблица, которая необходима для решения ЛП"""
+        self.iter = 0
+        self.LeadingRow = 0
+        self.LeadingColumn = 0
+
+        self.aVar = variables  # Amount of Variables
+        self.aIne = inequalities  # Amount of Inequalities
+
+        self.aRow = int(inequalities + 1)  # Amount of Rows
+        self.aCol = int(variables + self.aRow)  # Amount of Columns
+        self.table = zeros((self.aRow, self.aCol))  # Таблица необходимая для решения
+
+    def add_objective_function(self, list_of_elements: list) -> None:
+        """Добавляет коэффициенты целевой функции в таблицу"""
+        for i in range(len(list_of_elements)):
+            self.table[0][i] = -int(list_of_elements[i])
+
+    def add_inequality_function(self, list_of_elements: list, row_num: int) -> None:
+        """Добавляет коэффициенты и константу ОДНОГО неравенства в таблицу"""
+        for i in range(self.aVar):
+            self.table[row_num + 1][i] = int(list_of_elements[i])
+        self.table[row_num + 1][self.aVar + row_num] = 1
+        self.table[row_num + 1][-1] = int(list_of_elements[-1])
+
+    def run_simplex_method(self):
+        while self.iter == 0:
+            min_el = [self.table[0][0], 0]
+            for i in range(self.aRow):
+                if self.table[0][i] < min_el[0] and self.table[0][i] < 0:
+                    min_el[0] = self.table[0][i]
+                    min_el[1] = i
+            if min_el[0] >= 0:
+                print('Текущее базисное решение оптимально!')
+                self.iter = 1
+            else:
+                self.LeadingColumn = min_el[1]
+                self._check_have_inf_solutions()
+
+    def _check_have_inf_solutions(self):
+        p = 1
+        for i in range(1, len(self.table)):
+            if self.table[i][self.LeadingColumn] > 0:
+                p = 0
+            else:
+                print('Значение задачи ЛП не ограничено!')
+                self.iter = 2
+        if p != 1:
+            min_el = [self.table[1][-1] / self.table[1][self.LeadingColumn], 1]
+            for i in range(1, len(self.table)):
+                if self.table[i][self.LeadingColumn] > 0:
+                    a = self.table[i][-1] / self.table[i][self.LeadingColumn]
+                    if min_el[0] > a:
+                        min_el[0] = a
+                        min_el[1] = i
+
+            self.LeadingRow = min_el[1]
+            self._change_table()
+
+    def _change_table(self):
+        table = zeros((self.aRow, self.aCol))
+
+        lead_el = self.table[self.LeadingRow][self.LeadingColumn]  # Leading element - element on Leading Row and Leading Column
+        for i in range(self.aRow):
+            if i != self.LeadingRow:
+                for j in range(self.aCol-1):
+                    table[i][j] = self.table[i][j] - (self.table[self.LeadingRow][j] / lead_el * self.table[i][self.LeadingColumn])
+                table[i][self.aCol-1] = self.table[i][self.aCol-1] - (self.table[self.LeadingRow][self.aCol-1] / lead_el * self.table[i][self.LeadingColumn])
+            else:
+                for j in range(self.aCol - 1):
+                    table[i][j] = self.table[i][j] / lead_el
+                table[i][self.aCol-1] = self.table[i][self.aCol-1] / lead_el
+        print(table)
+        self.table = table
+
+    def get_table(self):
+        """просто выдает текущую таблицу"""
+        return self.table
+
+    def get_max(self):
+        """Выводит максимум"""
+        if self.iter != 0:
+            return self.table[0][-1]
+        else:
+            print("Сперва необходимо запустить метод")
+
+    def get_max_args(self):
+        """Выводит координаты максимума"""
+        if self.iter != 0:
+            result = list()
+            for i in range(self.aRow):
+                for j in range(self.aRow):
+                    if self.table[j][i] == 1:
+                        result.append(self.table[j][-1])
+                        break
+            return result
+        else:
+            print("Сперва необходимо запустить метод")
--- main.py
+++ main.py
@@ -1,100 +1,31 @@
-import numpy as numpy
-
-
-def main():
-    inf = open('C:/Users/Хэшэгто/Desktop/input.txt', 'r')
-    coeffs = int(inf.readline().strip())
-    inequalities = int(inf.readline().strip())
-    print('Количество переменных: ', coeffs)
-    print('Количество ограничений: ', inequalities)
-
-    method = SymplexMethod()
-    method.generateMatrix(coeffs, inequalities)
-
-    line = inf.readline().strip()
-    method.add_ObjectiveFunction(line, coeffs)
-
-    for i in range(inequalities):
-        line = inf.readline().strip()
-        method.add_IneqFunctions(line, coeffs, i)
-    inf.close()
-
-    print(method.table)
-    while method.iter == 0:
-        method.check_Optimality(coeffs)
-    for i in range(inequalities + 1):
-        print(method.table[i][-1])
-
-
-class SymplexMethod:
-    iter = 0
-    def generateMatrix(self, variables, constants):
-        self.table = numpy.zeros((constants + 1, variables + constants + 1))
-        self.signs = []
-
-    def add_ObjectiveFunction(self, eq, co):
-        s = eq.split()
-        for i in range(co):
-            self.table[0][i] = -int(s[i])
-
-    def add_IneqFunctions(self, ineq, co, num):
-        s = ineq.split()
-        for i in range(co):
-            self.table[num + 1][i] = s[i]
-        self.table[num + 1][co + num] = 1
-        self.table[num + 1][-1] = s[-1]
-        self.signs += [s[-2]]
-
-    def check_Optimality(self, co):
-        min = [self.table[0][0], 0]
-        for i in range(co):
-            if self.table[0][i] < min[0] and self.table[0][i] < 0:
-                min[0] = self.table[0][i]
-                min[1] = i
-        if min[0] >= 0:
-            print('Текущее базисное решение оптимально!')
-            self.iter = 1
-        else:
-            self.LeadingColumn = min[1]
-            self.check_InfSolutions()
-
-    def check_InfSolutions(self):
-        p = 1
-        for i in range(1, len(self.table)):
-            if self.table[i][self.LeadingColumn] > 0:
-                p = 0
-            else:
-                print('Значение задачи ЛП не ограничено!')
-                self.iter = 2
-        if p != 1:
-            min = [self.table[1][-1] / self.table[1][self.LeadingColumn], 1]
-            for i in range(1, len(self.table)):
-                if self.table[i][self.LeadingColumn] > 0:
-                    a = self.table[i][-1] / self.table[i][self.LeadingColumn]
-                    if min[0] > a:
-                        min[0] = a
-                        min[1] = i
-
-            self.LeadingString = min[1]
-            self.tableConversion()
-
-    def tableConversion(self):
-        n = len(self.table[0])
-        m = len(self.table)
-        table = numpy.zeros((m, n))
-        a = self.table[self.LeadingString][self.LeadingColumn]
-        for i in range(m):
-            if i != self.LeadingString:
-                for j in range(n-1):
-                    table[i][j] = self.table[i][j] - (self.table[self.LeadingString][j] / a * self.table[i][self.LeadingColumn])
-                table[i][n-1] = self.table[i][n-1] - (self.table[self.LeadingString][n-1] / a * self.table[i][self.LeadingColumn])
-            else:
-                for j in range(n - 1):
-                    table[i][j] = self.table[i][j] / a
-                table[i][n-1] = self.table[i][n-1] / a
-        print(table)
-        self.table = table
+from SimplexMethod import SimplexMethod
 
 
 if __name__ == "__main__":
-    main()
+    inf = open('input.txt', 'r')
+    # первая цифра отвечает за кол-во переменных, за кол-во Х-ов
+    variables = int(inf.readline().strip())
+
+    # вторая цифра отвечает за кол-во неравенств, все они должны быть приведены к виду <= и их коэф-ты записаны в файле
+    inequalities = int(inf.readline().strip())
+
+    print('Количество переменных: ', variables)
+    print('Количество ограничений: ', inequalities)
+
+    task = SimplexMethod(variables, inequalities)
+
+    # третья строка должна содержать коэф-ты целевой функции (с этой функцией не надо производить никаких манипуляций)
+    line = inf.readline().strip().split()
+    task.add_objective_function(line)
+
+    # все остальные строки содержат коэфициенты неравенств, приведеных к виду <=, константы остаются справа
+    for i in range(inequalities):
+        line = inf.readline().strip().split()
+        task.add_inequality_function(line, i)
+    inf.close()
+
+    print(task.get_table())
+    task.run_simplex_method()
+
+    print("Максимум ", task.get_max())
+    print("Он достигается в координатах ", task.get_max_args())
